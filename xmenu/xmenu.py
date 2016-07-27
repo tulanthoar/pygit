00d053157f4a0a86bc53f2665b73f5123be55fee
Nate Yonkee <tulanthoar@gmail.com>
2016-07-27 17:02:21 -0600

    simplify the byobu functions

--- xmenu/xmenu.py
+++ xmenu/xmenu.py
@@ -5,24 +5,25 @@ from sh import Command
 from ewmh import EWMH
 
 
-def class_is_mapped(hinter, class_name):
-    '''use ewmh to see if a window with class name is mapped'''
-    for win in hinter.getClientListStacking():
-        classnames = win.get_wm_class()
-        if class_name in classnames:
-            return True
-    return False
+# def class_is_mapped(hinter, class_name):
+#     '''use ewmh to see if a window with class name is mapped'''
+#     for win in hinter.getClientListStacking():
+#         classnames = win.get_wm_class()
+#         if class_name in classnames:
+#             return True
+#     return False
 
 
 def class_is_visible(hinter, class_name):
     '''check if a window with class_name is currently visible'''
     for win in hinter.getClientListStacking():
         if class_name in win.get_wm_class():
-            attrs = win.get_attributes()
-            if attrs._data['map_state'] == 2:
-                return True
-    return False
+            return win.get_wm_state()['state'] == 1
+    return None
 
+                # return True
+            # else:
+                # return False
 
 def print_menu(persist):
     '''print menu of available keys'''
@@ -83,14 +84,14 @@ def xdomenu():
         else:
             xmc('suicide')
         if char == 'b':
-            if not class_is_mapped(hinter, 'urxv'):
+            vis = class_is_visible(hinter, 'urxv')
+            if vis is None:
                 xmc('byobu')
                 sleep(1)
+            elif vis:
+                xmc('sendbyo')
             else:
-                if class_is_visible(hinter, 'urxv'):
-                    xmc('sendbyo')
-                else:
-                    xmc('bringbyo')
+                xmc('bringbyo')
         else:
             (opts) = char_to_bin[char]
             xmc(opts)
