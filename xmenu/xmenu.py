3e2dc37ad679c50e0967280d2a4164bdf5a1fea6
Nate Yonkee <tulanthoar@gmail.com>
2016-07-31 10:51:10 -0600

    fix spotaneous death and adjust to byobu NScratch

--- xmenu/xmenu.py
+++ xmenu/xmenu.py
@@ -2,31 +2,23 @@
 from time import sleep
 from click import echo, getchar, command
 from sh import Command
-from ewmh import EWMH
-
-
-def class_is_visible(hinter, class_name):
-    '''check if a window with class_name is currently visible'''
-    for win in hinter.getClientListStacking():
-        if class_name in win.get_wm_class():
-            return win.get_wm_state()['state'] == 1
-    return None
 
 
 def print_menu(persist):
     '''print menu of available keys'''
     echo('Spc: next empty ws')
     echo('Tab: toggle persist')
+    echo('  a: all pads')
+    echo('  b: byobu')
     echo('  c: clipmenu')
-    echo('  q: qutebrowser')
+    echo('  h: htop')
+    echo('  i: ipython')
     echo('  j: j4-app-menu')
     echo('  n: neovim')
-    echo('  i: ipython')
+    echo('  q: qutebrowser')
     echo('  p: perl repl')
-    echo('  r: ranger')
-    echo('  a: all pads')
     echo('  P: pomodoro')
-    echo('  h: htop')
+    echo('  r: ranger')
     echo('  u: urxvt')
     if persist:
         echo('Persistence on')
@@ -39,6 +31,7 @@ def xdomenu():
     """interacts with a simple menu."""
     xmc = Command('xmctl')
     char_to_bin = {'q': ('srmenu'),
+                   'b': ('byobu'),
                    'c': ('clipmenu'),
                    'j': ('jmenu'),
                    'n': ('nvim'),
@@ -50,19 +43,15 @@ def xdomenu():
                    'a': ('allpads'),
                    'P': ('pomodoro')}
     xdo = Command('xdotool')
-    hinter = EWMH()
-    persistent = True
+    persistent = False
     print_menu(persistent)
     while True:
         char = getchar()
         try:
             (opts) = char_to_bin[char]
-            if persistent:
-                xmc('minone')
-            else:
-                xmc('copykill')
+            xmc('minone')
             xmc(opts)
-        except (KeyError):
+        except KeyError:
             if char == '\t':
                 persistent = not persistent
                 print_menu(persistent)
@@ -70,19 +59,6 @@ def xdomenu():
             elif char == ' ':
                 xmc('nextempty')
                 continue
-            if char == 'b':
-                if persistent:
-                    xmc('minone')
-                else:
-                    xmc('copykill')
-                vis = class_is_visible(hinter, 'urxv')
-                if vis is None:
-                    xmc('byobu')
-                    sleep(1)
-                elif vis:
-                    xmc('sendbyo')
-                else:
-                    xmc('bringbyo')
         sleep(0.5)
         if persistent:
             xmc('rest')
