638dcd99657d853c072d9a1ec6ff21bdd8231dde
Nate <tulanthoar@gmail.com>
2016-11-12 23:57:30 -0700

    update menu to new changes in xmonad

--- xmenu/xmenu.py
+++ xmenu/xmenu.py
@@ -6,7 +6,7 @@ from click import getchar, command
 from pykeyboard import PyKeyboard
 
 
-def print_flush(text, time=0.01):
+def print_flush(text, time=0.001):
     '''print then-flush-for-xmctl'''
     print(text)
     sleep(time)
@@ -18,17 +18,11 @@ def print_menu(persist):
     print_flush('Available cmds')
     print_flush('Spc: next empty ws')
     print_flush('Tab: toggle persist')
-    print_flush('  a: all pads')
-    print_flush('  b: byobu')
     print_flush('  c: clipmenu')
-    print_flush('  h: htop')
-    print_flush('  i: ipython')
     print_flush('  j: j4-app-menu')
-    print_flush('  n: neovim')
-    print_flush('  q: qutebrowser')
-    print_flush('  p: perl repl')
-    print_flush('  P: pomodoro')
-    print_flush('  r: ranger')
+    print_flush('  s: search-engines')
+    print_flush('  p: pomodoro')
+    print_flush('  t: konsole')
     print_flush('  u: urxvt')
     if persist:
         print_flush('Persistence on')
@@ -39,33 +33,24 @@ def print_menu(persist):
 @command()
 def xdomenu():
     """interacts with a simple menu."""
-    char_to_bin = {'q': 'srmenu',
-                   'b': 'byobu',
-                   'c': 'clipmenu',
+    char_to_bin = {'s': 'srmenu',
                    'j': 'jmenu',
-                   'n': 'nvim',
-                   'h': 'htop',
-                   'u': 'myterm',
-                   'i': 'ipython',
-                   'p': 'perl',
-                   'r': 'ranger',
-                   ' ': 'moveempty',
-                   'a': 'allpads',
-                   'P': 'pomodoro'}
+                   'c': 'clipmenu',
+                   't': 'terminal',
+                   'u': 'urxvt',
+                   'p': 'pomodoro',
+                   ' ': 'moveempty'}
     keybrd = PyKeyboard()
     k_menu = keybrd.menu_key
     persistent = False
     print_menu(persistent)
     while True:
-        sleep(0.5)
+        sleep(0.1)
         stdout.flush()
         char = getchar()
         try:
             cmd = char_to_bin[char]
-            # keybrd.tap_key(k_menu)
-            print(cmd)
-            sleep(0.15)
-            stdout.flush()
+            print_flush(cmd)
             if persistent:
                 sleep(0.2)
                 keybrd.tap_key(k_menu)
