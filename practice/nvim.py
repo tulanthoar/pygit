118f2b44fb25f6c750ad3c0f0fb2fcea1311b7ef
Nathan Yonkee <tulanthoar@gmail.com>
2017-01-01 17:24:32 -0700

    practice pys

---
+++ practice/nvim.py
@@ -0,0 +1,14 @@
+'''testing out ctags on python'''
+
+
+def printlower():
+    '''just print some words'''
+    sen = "I'm some words"
+    sen = sen.upper()
+    sen.lower()
+    print(sen)
+    return True
+
+
+if __name__ == '__main__':
+    printlower()
--- practice/ssh.py
+++ practice/ssh.py
@@ -1,19 +1,23 @@
-import os, sys
+'''maybe try to connect to ssh server'''
+import sys
+from sys import stdin, stdout
+import os
 
 from sh import ssh
 
-print( "1" )
+
 #  open stdout in unbuffered mode
 
-aggregated = ""
-def ssh_interact( char, stdin ):
-    global aggregated
-    sys.stdout.write( char.encode() )
-    aggregated += char
-    if aggregated.endswith( "password: " ):
-        stdin.put( "correcthorsebatterystaple\n" )
-print( 'abc' )
-sys.stdout = os.fdopen( sys.stdout.fileno(), "wb", 0 )
-p = ssh( "10.10.10.100", _out = ssh_interact, _out_bufsize = 0, _tty_in = True )
-print( 'bcd' )
 
+def ssh_interact(char, aggregated):
+    """automate connecting to an ssh server"""
+    stdout.write(char.encode())
+    aggregated += char
+    if aggregated.endswith("password: "):
+        stdin.write("correcthorsebatterystaple\n")
+        _out = os.fdopen(sys.stderr, "wb", 0)
+        ssh("9.10.10.100", _out=ssh_interact, _out_bufsize=0, _tty_in=True)
+    return _out
+
+if __name__ == '__main__':
+    print(ssh_interact("a", ""))
